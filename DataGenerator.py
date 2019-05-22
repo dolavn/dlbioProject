d15cefe31087ee544d60d4a4c241f7cb1ee9e3a3
dinasv <dinasv@post.bgu.ac.il>
2019-05-22 12:50:28 +0300

    Change DataGenerator to recieve kernel_size and max_size, and add read files and sample encoding functions

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,62 +1,39 @@
 import numpy as np
 import keras
-max_size = 45
-kernel_size = 12
-
-reverse_compliment_base = {'A': 'U', 'C': 'G', 'T': 'A', 'G': 'C', 'N': 'N'}
 
 
-def reverse_compliment(string):
-    return string.replace('T', 'U')
-    string = [reverse_compliment_base[base] for base in string[::-1]]
-    return ''.join(string)
+# max_size = 45
+# kernel_size = 12
 
-
-def pad(string, max_size):
-    string += 'N' * (max_size-len(string))
-    return string
-
-
-def pad_conv(string, kernel_size):
-    pad = 'N'*(kernel_size-1)
-    string = pad + string + pad
-    return string
-
-
-def one_hot(string):
-    dict = {'A': np.array([1, 0, 0, 0]), 'G': np.array([0, 1, 0, 0]),
-            'C': np.array([0, 0, 1, 0]), 'U': np.array([0, 0, 0, 1]),
-            'N': np.array([0.25]*4)}
-    vec_list = [dict[c].reshape(1, -1) for c in pad_conv(pad(string, max_size), kernel_size)]
-    return np.concatenate(vec_list, axis=0).reshape(len(vec_list), 4, 1)
-
-
-def one_hot_encode(line):
-    return one_hot(reverse_compliment(line))
-
-
-def read_file(path, ind, list):
-    with open(path, 'r') as f:
-        for line in f:
-            seq = line.strip().split()[0]
-            list.append((seq, ind))
-
+# reverse_compliment_base = {'A': 'U', 'C': 'G', 'T': 'A', 'G': 'C', 'N': 'N'}
 
 class DataGenerator(keras.utils.Sequence):
-    def __init__(self, rbns_files, batch_size=32, dim=(max_size+2*kernel_size-2, 4, 1), shuffle=False):
+    def __init__(self, rbns_files, kernel_size, max_sample_size, batch_size=32, shuffle=False):
+        self.kernel_size = kernel_size
+        self.max_sample_size = max_sample_size
+
+        self.dim = (max_sample_size + 2 * kernel_size - 2, 4, 1)
         self.rbns_files = rbns_files
         self.batch_size = batch_size
         self.lines = []
+
         for ind, file in enumerate(rbns_files):
-            read_file(file, ind, self.lines)
+            self.read_file(file, ind)
+
         self.size = len(self.lines)
         self.shuffle = shuffle
-        self.dim = dim
         self.indexes = np.arange(self.size)
+
         self.on_epoch_end()
 
+    def read_file(self, path, ind):
+        with open(path, 'r') as f:
+            for line in f:
+                seq = line.strip().split()[0]
+                self.lines.append((seq, ind))
+
     def __len__(self):
-        return int(self.size/self.batch_size)
+        return int(self.size / self.batch_size)
 
     def __getitem__(self, index):
         indices = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
@@ -66,14 +43,40 @@ class DataGenerator(keras.utils.Sequence):
     def on_epoch_end(self):
         self.indexes = np.arange(self.size)
         if self.shuffle:
-            self.indexes = np.random.shuffle(self.indexes)
+            np.random.shuffle(self.indexes)
 
     def _data_generation(self, indices_list):
         x = np.empty((self.batch_size, *self.dim))
         y = np.empty((self.batch_size, len(self.rbns_files)))
         for i, ind in enumerate(indices_list):
-            curr_x = one_hot_encode(self.lines[ind][0])
-            x[i, ] = curr_x
+            curr_x = self.one_hot_encode(self.lines[ind][0])
+            x[i,] = curr_x
             y[i] = [1 if j == self.lines[ind][1] else 0 for j in range(len(self.rbns_files))]
         return x, y
 
+    @staticmethod
+    def reverse_compliment(string):
+        return string.replace('T', 'U')
+        # string = [reverse_compliment_base[base] for base in string[::-1]]
+        # return ''.join(string)
+
+    def pad(self, string):
+        string += 'N' * (self.max_sample_size - len(string))
+        return string
+
+    def pad_conv(self, string):
+        pad = 'N' * (self.kernel_size - 1)
+        string = pad + string + pad
+        return string
+
+    def one_hot(self, string):
+        encoding = {'A': np.array([1, 0, 0, 0]), 'G': np.array([0, 1, 0, 0]),
+                'C': np.array([0, 0, 1, 0]), 'U': np.array([0, 0, 0, 1]),
+                'N': np.array([0.25] * 4)}
+
+        padded_string = self.pad(string)
+        vec_list = [encoding[c].reshape(1, -1) for c in self.pad_conv(padded_string)]
+        return np.concatenate(vec_list, axis=0).reshape(len(vec_list), 4, 1)
+
+    def one_hot_encode(self, line):
+        return self.one_hot(DataGenerator.reverse_compliment(line))
--- Reader.py
+++ Reader.py
@@ -5,32 +5,16 @@ import sys
 import matplotlib.pyplot as plt
 import keras
 from keras.models import Sequential
-from keras.preprocessing import sequence
+
 from keras.layers import Dense, Dropout, Activation, Flatten, Embedding
 from keras.layers import Conv2D, MaxPooling2D, SimpleRNN
 from keras import initializers
-from keras.utils import to_categorical
+
 from DataGenerator import DataGenerator
-PATH = 'RBNS_example/'
-PATH = './'
+PATH = 'RBP1_example/'
+#PATH = './'
 kernel_size = 12
 max_size = 45
-reverse_compliment_base = {'A': 'U', 'C': 'G', 'T': 'A', 'G': 'C', 'N': 'N'}
-
-
-def reverse_compliment(string):
-    return string.replace('T', 'U')
-    string = [reverse_compliment_base[base] for base in string[::-1]]
-    return ''.join(string)
-
-
-def one_hot(string):
-    dict = {'A': np.array([1, 0, 0, 0]), 'G': np.array([0, 1, 0, 0]),
-            'C': np.array([0, 0, 1, 0]), 'U': np.array([0, 0, 0, 1]),
-            'N': np.array([0.25]*4)}
-    vec_list = [dict[c].reshape(1, -1) for c in string]
-    return np.concatenate(vec_list, axis=0).reshape(len(string), 4, 1)
-
 
 def get_files_list(rbp_ind):
     lst = []
@@ -48,39 +32,6 @@ def get_files_list(rbp_ind):
 
     lst.sort(key=lambda x: x[1])
     return [file for file, cons in lst]
-
-
-def pad(string, max_size):
-    string += 'N' * (max_size-len(string))
-    return string
-
-
-def pad_conv(string, kernel_size):
-    pad = 'N'*(kernel_size-1)
-    string = pad + string + pad
-    return string
-
-
-def get_str_dict(seqs_lists):
-    str_dict = {}
-    for lst_ind, seqs in enumerate(seqs_lists):
-        for seq, count in seqs:
-            if seq in str_dict:
-                str_dict[seq]['y'][lst_ind] = 1
-            else:
-                str_dict[seq] = {'x': one_hot(pad_conv(pad(seq, max_size), kernel_size)),
-                                 'y': [1 if i == lst_ind else 0 for i in range(len(seqs_lists))]}
-    return list(str_dict.values())
-
-
-def get_x(str_dict):
-    x = [entry['x'] for entry in str_dict]
-    return x
-
-
-def get_y(str_dict):
-    y = [entry['y'] for entry in str_dict]
-    return y
 
 
 def read_file_rbns(path, max_len=-1):
@@ -144,23 +95,6 @@ def create_model_rnn(x_train):
     return model
 
 
-def fit_model(model):
-    batch_size = 32
-
-    # initiate RMSprop optimizer
-    opt = keras.optimizers.rmsprop(lr=0.0005, decay=1e-6)
-
-    # Let's train the model using RMSprop
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt,
-                  metrics=['accuracy'])
-
-    model.fit(np.array(x_train), np.array(y_train),
-              batch_size=batch_size,
-              epochs=20,
-              shuffle=True)
-
-
 def load_files(args):
     if len(args) != 3:
         raise BaseException("Not enough arguments")
@@ -186,8 +120,8 @@ def calc_corr(seqs, y_test, y_pred):
 
 if __name__ == '__main__':
     print('Starting')
-    l, cmpt_file = load_files(sys.argv)
-    d = DataGenerator(l)
+    l, cmpt_seqs = load_files(sys.argv)
+    d = DataGenerator(l, kernel_size, max_size)
     model = create_model(d.dim)
     opt = keras.optimizers.rmsprop(lr=0.0005, decay=1e-6)
 
@@ -199,9 +133,9 @@ if __name__ == '__main__':
                         use_multiprocessing=True,
                         workers=10)
 
-    seqs = [pad_conv(pad(seq, max_size), kernel_size) for seq in cmpt_file]
+    #seqs = [d.pad_conv(d.pad(seq)) for seq in cmpt_file]
 
-    x_test = np.array([one_hot(seq) for seq in seqs])
+    x_test = np.array([d.one_hot(seq) for seq in cmpt_seqs])
     y_test = [int(x) for x in np.append(np.ones(1000), np.zeros(len(x_test) - 1000), axis=0)]
     y_pred = model.predict(x_test)
-    calc_corr(seqs, y_test, y_pred)
+    calc_corr(cmpt_seqs, y_test, y_pred)
