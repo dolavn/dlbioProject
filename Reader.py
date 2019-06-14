7079d8c056f1da107ecff4e52627c8b89514f051
dinasv <dinasv@post.bgu.ac.il>
2019-06-14 11:23:37 +0300

    Change auc computation

--- Reader.py
+++ Reader.py
@@ -24,35 +24,46 @@ is_binary_model = True
 use_shuffled_seqs = True
 
 '''data parameters'''
-valid_p = 0
+valid_p = 0.2
 max_size = 45
-file_limit = 200000
+file_limit = 250000
 
 '''model parameters'''
-kernel_size = 8
-num_of_kernels = 64
-dense1_layer_size = 32
-dense2_layer_size = 8
+kernel_size = 9
+num_of_kernels = 60
+dense_layers = [40]
+dense1_layer_size = 40
+dense2_layer_size = 0
+dense3_layer_size = 0
 
 '''fit parameters'''
-batch_size = 264
-epochs = 2
+batch_size = 256
+epochs = 4
 workers = 1
 
-model_path += '_samp{}_kernel{}_{}_dense1_{}_dense2_{}_batch{}_epoch{}_shuf_{}'.format(file_limit, kernel_size,
+dim = (max_size + 2 * kernel_size - 2, 4, 1)
+
+model_path += '_samp{}_kernel{}_{}_dense1_{}_dense2_{}_batch{}_epoch{}_shuf_{}_binary{}'.format(file_limit, kernel_size,
                                                                                       num_of_kernels,
                                                                                       dense1_layer_size,
                                                                                       dense2_layer_size,
                                                                                       batch_size, epochs,
-                                                                                      use_shuffled_seqs)
+                                                                                      use_shuffled_seqs,
+                                                                                        is_binary_model)
 
 
 class RBNSreader():
     @staticmethod
     def read_files(rbns_files, file_limit=None):
+        #file_limit2 = file_limit
+        #if file_limit != None:
+            #file_limit2 = file_limit * 4
         lines_from_all_files = []
         for ind, file in enumerate(rbns_files):
-            lines_from_all_files += RBNSreader.read_file(file, ind + 1, file_limit)
+            lines = RBNSreader.read_file(file, ind + 1, file_limit)
+            #np.random.shuffle(lines)
+            #lines_from_all_files += lines[:file_limit]
+            lines_from_all_files += lines[:file_limit]
 
         return lines_from_all_files
 
@@ -60,13 +71,20 @@ class RBNSreader():
     def read_file(path, ind, file_limit):
         lines = []
         count = 0
+        more_than_one_occ = 0
         with open(path, 'r') as f:
             for line in f:
-                seq = line.strip().split()[0]
-                lines.append((seq, ind))
-                count += 1
-                if file_limit and count >= file_limit:
+                seq, val = line.strip().split()
+
+                #if int(val) > 1:
+                #    more_than_one_occ += 1
+                #if count % 30 == 0:
+                #    lines.append((seq, ind))
+                #count += 1
+                if file_limit and len(lines) >= file_limit:
                     break
+
+        print('more than one occ', more_than_one_occ)
         return lines
 
 
@@ -117,13 +135,11 @@ def create_model(dim, num_classes):
     model.add(Activation('relu'))
     model.add(GlobalMaxPooling2D())
 
-    model.add(Dense(dense1_layer_size))
-    model.add(Activation('relu'))
-
-    if dense2_layer_size > 0:
-        model.add(Dense(dense2_layer_size))
+    for dense_size in dense_layers:
+        model.add(Dense(dense_size))
         model.add(Activation('relu'))
-        # model.add(Dropout(0.25))
+
+    #model.add(Dropout(0.25))
 
     model.add(Dense(num_classes))
     model.add(Activation('sigmoid'))
@@ -144,38 +160,28 @@ def load_files(args):
         return files, cmpt_file, rbp_num
 
 
-def calc_corr(seqs, y_test, y_pred, num_classes):
+def calc_auc(y_pred):
+
     if is_binary_model:
         y_pred_scores = [y[0] for y in y_pred]
     else:
-        y_pred_scores = [np.dot(y, np.concatenate((np.ones(1) * -10, np.ones(num_classes - 1)))) for y in y_pred]
+        print(y_pred[0])
+        print(np.concatenate((np.ones(1) * -10, np.array([10, 100]))))
+        y_pred_scores = [np.dot(y, np.concatenate((np.ones(1) * -10, np.array([20, 10])))) for y in y_pred]
 
-    # print(y_pred_scores)
-    x_test_y_pred = list(zip(seqs, y_pred_scores, list(range(len(seqs)))))
-    np.random.shuffle(x_test_y_pred)
-    x_test_y_pred_sorted = sorted(x_test_y_pred, key=lambda x: x[1], reverse=True)
-    # print(x_test_y_pred_sorted[:20])
-    x_test_y_pred_tagged = [(seq, tag, int(ind)) for (seq, score, ind), tag in zip(x_test_y_pred_sorted, y_test)]
-    x_test_y_pred_tagged = sorted(x_test_y_pred_tagged, key=lambda x: x[2])
+    true = [int(x) for x in np.append(np.ones(1000), np.zeros(len(x_test) - 1000), axis=0)]
 
-    # print(x_test_y_pred_tagged[:20])
-    positives = [tag for (seq, tag, ind) in x_test_y_pred_tagged[:1000]]
-
-    print('positives', sum(positives))
-
-    avg_precision = average_precision_score([tag for (seq, tag, ind) in x_test_y_pred_tagged], [int(x) for x in
-                                                                                                np.append(np.ones(1000),
-                                                                                                          np.zeros(len(
-                                                                                                              x_test) - 1000),
-                                                                                                          axis=0)])
+    avg_precision = average_precision_score(true, y_pred_scores)
     print('avg_precision', avg_precision)
 
     plt.figure(figsize=(20, 10))
     plt.scatter(range(1, len(y_pred_scores) + 1), y_pred_scores)
     plt.savefig('scores.png')
 
+    '''
     with open('positives.txt', 'w') as f:
         f.write('\n'.join([str(i) for i in range(len(positives)) if positives[i] == 1]))
+    '''
 
 
 def create_negative_seqs(lines_from_all_files):
@@ -187,12 +193,12 @@ def create_negative_seqs(lines_from_all_files):
     return negative_seqs
 
 
-def create_train_valid_data(files):
-    lines_from_all_files = RBNSreader.read_files(files[1:], file_limit=file_limit)
+def create_train_valid_data(negative_file, positive_files):
+    lines_from_all_files = RBNSreader.read_files(positive_files, file_limit=file_limit)
     if use_shuffled_seqs:
         lines_from_all_files += create_negative_seqs(lines_from_all_files)
     else:
-        lines_from_all_files += RBNSreader.read_file(files[0], 0, file_limit * 2)
+        lines_from_all_files += RBNSreader.read_file(negative_file, 0, file_limit)
 
     np.random.shuffle(lines_from_all_files)
 
@@ -218,23 +224,24 @@ def create_train_valid_data(files):
 
     return train_gen, valid_gen
 
+def create_and_compile_model(dim, num_of_classes, loss_func):
 
-def train_model(files, loss_func='categorical_crossentropy'):
-    train_gen, valid_gen = create_train_valid_data(files)
-    model = create_model(train_gen.dim, num_of_classes)
-    opt = keras.optimizers.Adam()
+    model = create_model(dim, num_of_classes)
+    opt = keras.optimizers.Adam(lr=0.01)
 
-    # Let's train the model using RMSprop
     model.compile(loss=loss_func,
                   optimizer=opt,
                   metrics=['accuracy'])
+
+    return model
+
+def train_model(model, train_gen, valid_gen):
+
     model.fit_generator(generator=train_gen,
                         validation_data=valid_gen,
                         use_multiprocessing=True,
                         epochs=epochs,
                         workers=workers)
-
-    model.save(model_path)
 
     return model
 
@@ -247,13 +254,14 @@ if __name__ == '__main__':
     files, cmpt_seqs, rbp_num = load_files(sys.argv)
 
     print(files)
-    files = [files[0]] + files[-1:]
+    files = [files[0]] + files[-2:]
     print(files)
     model_path += '_rbp' + str(rbp_num) + 'files_' + '_'.join([str(cons) for file, cons in files[1:]])
     print(model_path)
 
     files = [file for file, cons in files]
-    num_of_classes = len(files) + 1
+    num_of_classes = len(files)
+    print('classes', num_of_classes)
     loss_func = 'categorical_crossentropy'
     if is_binary_model:
         num_of_classes = 1
@@ -261,14 +269,22 @@ if __name__ == '__main__':
 
     exists = os.path.isfile(model_path)
     if exists:
+        print('loading model')
         model = keras.models.load_model(model_path)
     else:
-        model = train_model(files, loss_func)
+
+        model = create_and_compile_model(dim, num_of_classes, loss_func)
+
+        print('training model')
+        #for file in files[1:]:
+        train_gen, valid_gen = create_train_valid_data(negative_file=files[0], positive_files=files[1:])
+        model = train_model(model, train_gen, valid_gen)
+        model.save(model_path)
 
     x_test = np.array([DataGenerator.one_hot(seq, max_size, kernel_size) for seq in cmpt_seqs])
     y_test = [int(x) for x in np.append(np.ones(1000), np.zeros(len(x_test) - 1000), axis=0)]
     y_pred = model.predict(x_test)
-    calc_corr(cmpt_seqs, y_test, y_pred, num_of_classes)
+    calc_auc(y_pred)
 
     end = time.time()
     print('took', (end - start) / 60, 'minutes')
